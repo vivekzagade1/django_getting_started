c82e16d8b066961d986f0ddb9cdb6d1f37e04339
Hrishikesh Zagade <hrishi.zagade@gmail.com>
2023-09-14 21:55:17 +0530

    14-9-23

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404
 
-from meetings.models import Meeting
+from meetings.models import Meeting, Room
 
 
 def detail(request, id):
-    #meeting = Meeting.objects.get(pk=id)
+    # meeting = Meeting.objects.get(pk=id)
     meeting = get_object_or_404(Meeting, pk=id)
     return render(request, "meetings/detail.html", {
         "meeting": meeting
+    })
+
+
+def rooms_list(request):
+    rooms = Room.objects.all()
+    return render(request, "meetings/rooms_list.html", {
+        "rooms": rooms
     })
